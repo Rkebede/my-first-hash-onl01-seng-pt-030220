57979eb60fc1aa0880f883aebf46bc6f56d8d43a
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 18:38:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,6 @@
-def my_hash {"apples" => "5" , "clothing" => "10"}
+def my_hash {
+  "apples" => "5" , 
+  "clothing" => "10"}
   
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
